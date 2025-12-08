998d9a538a7aa23738508301d20726c566834eac
Frank Harkins <frankharkins@hotmail.co.uk>
2025-12-08 15:49:02 +0000

    Add comment for placeholder code

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 
 use napi_derive::napi;
 
+/// This is just a placeholder to check the tests are working. We'll replace
+/// this with code soon.
 #[napi]
 pub fn plus_100(input: u32) -> u32 {
   input + 100
