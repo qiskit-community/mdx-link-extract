17e94afe0aeef0cdf67477b4f42a61abbe337eb1
Frank Harkins <frankharkins@hotmail.co.uk>
2026-01-27 17:12:02 +0000

    Refactor: Rename and move functions around

--- src/anchors/mod.rs
+++ src/anchors/mod.rs
@@ -1,20 +1,21 @@
 use markdown::mdast::{AttributeContent, AttributeValue, Heading, Node};
 use std::collections::HashMap;
 
+/// Given any markdown node, extract the
 pub fn extract_from_node(node: &Node, anchor_occurrences: &mut HashMap<String, u32>) {
   match node {
     Node::Heading(h) => {
-      let anchor = extract_from_heading(h);
+      let anchor = anchor_from_heading(h);
       let existing_duplicates = anchor_occurrences.get(&anchor).unwrap_or(&0);
       anchor_occurrences.insert(anchor, *existing_duplicates + 1);
     }
     Node::MdxJsxFlowElement(el) => {
-      if let Some(anchor) = extract_from_attributes(&el.attributes) {
+      if let Some(anchor) = get_id_prop(&el.attributes) {
         anchor_occurrences.insert(anchor, 1);
       };
     }
     Node::MdxJsxTextElement(el) => {
-      if let Some(anchor) = extract_from_attributes(&el.attributes) {
+      if let Some(anchor) = get_id_prop(&el.attributes) {
         anchor_occurrences.insert(anchor, 1);
       };
     }
@@ -22,7 +23,19 @@ pub fn extract_from_node(node: &Node, anchor_occurrences: &mut HashMap<String, u
   };
 }
 
-fn extract_from_attributes(attributes: &Vec<AttributeContent>) -> Option<String> {
+pub fn deduplicate_anchors(anchor_occurrences: HashMap<String, u32>) -> Vec<String> {
+  anchor_occurrences
+    .into_iter()
+    .flat_map(|(anchor, duplications)| {
+      (0..duplications).map(move |n| match n {
+        0 => format!("#{anchor}"),
+        n => format!("#{anchor}-{n}"),
+      })
+    })
+    .collect()
+}
+
+fn get_id_prop(attributes: &Vec<AttributeContent>) -> Option<String> {
   for attr in attributes.iter() {
     let AttributeContent::Property(prop) = attr else {
       continue;
@@ -37,15 +50,16 @@ fn extract_from_attributes(attributes: &Vec<AttributeContent>) -> Option<String>
   return None;
 }
 
-fn extract_from_heading(heading: &Heading) -> String {
+fn anchor_from_heading(heading: &Heading) -> String {
   let mut text = String::with_capacity(100);
   for child in heading.children.iter() {
-    extract_text(child, &mut text);
+    get_text(child, &mut text);
   }
   heading_to_anchor(text)
 }
 
-pub fn extract_text<'a>(node: &'a Node, s: &mut String) {
+/// Get plain text from a node and all its children
+pub fn get_text<'a>(node: &'a Node, s: &mut String) {
   let maybe_text = match node {
     Node::Text(text) => Some(&text.value),
     Node::InlineCode(text) => Some(&text.value),
@@ -57,21 +71,9 @@ pub fn extract_text<'a>(node: &'a Node, s: &mut String) {
 
   if let Some(children) = node.children() {
     for child in children {
-      extract_text(child, s);
+      get_text(child, s);
     }
   }
-}
-
-pub fn deduplicate_anchors(anchor_occurrences: HashMap<String, u32>) -> Vec<String> {
-  anchor_occurrences
-    .into_iter()
-    .flat_map(|(anchor, duplications)| {
-      (0..duplications).map(move |n| match n {
-        0 => format!("#{anchor}"),
-        n => format!("#{anchor}-{n}"),
-      })
-    })
-    .collect()
 }
 
 fn heading_to_anchor(heading: String) -> String {
